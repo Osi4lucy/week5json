9eb3fa16a4632ed3fe7844759373415b53e4df32
Osilamah <soloosilamah@yhaoo.com>
2017-11-15 12:59:52 +0100

    some changes lease enter the commit message for your changes. Lines starting

--- week5.js
+++ week5.js
@@ -1,6 +1,32 @@
 function myButton(){
   console.log("You clickd me!");
-}
-function fetchData ('https://api.github.com/orgs/HackYourFuture/repos', function(data){
-  console.log (Data);
-});
+  //const url = "https://api.github.com/users/" + username + "/repos";
+  const buttonElement = document.querySelector('.button');
+  const url = 'https://api.github.com/orgs/HackYourFuture/repos';
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', function (data) {
+      if (this.status === 200) {
+      const responseText = req.responseText;
+      const data = JSON.parse(responseText);
+    //  const logo = document.querySelector('#logo');
+    //  logo.src = repositoryList[0].owner.avatar_url;
+      console.log(data);
+  data.forEach(function (myLi){
+    const li = document.createElement('li');
+    const nameText=document.createTextNode(myLi.name);
+    li.appendChild(nameText);
+    hyfrepos.appendChild(li);
+  });
+  console.log(hyfrepos);
+
+  } else {
+          console.log('Something is probably wrong with the url');
+      }
+  });
+  req.addEventListener('error', function () {
+      console.log('Server error like timeout');
+  });
+
+  req.open("GET", url);
+  req.send();
+  console.log(buttonElement);
